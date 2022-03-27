87bf41035b1151242f84114f9db23df1b729041f
Amos Alagbe <sunboy005@gmail.com>
2022-03-27 22:13:17 +0100

    customer-AddingTransit-partly

--- Product.Microservice/Startup.cs
+++ Product.Microservice/Startup.cs
@@ -1,20 +1,14 @@
+using MassTransit;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 using Product.Microservice.Context;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using MediatR;
+using System;
 using System.Reflection;
 
 namespace Product.Microservice
@@ -31,7 +25,19 @@ namespace Product.Microservice
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddMassTransit(x =>
+                x.AddBus(provider => Bus.Factory.CreateUsingRabbitMq(configuration =>
+                 {
+                     configuration.UseHealthCheck(provider);
+                     configuration.Host(new Uri("rabbitmq://localhost"), h =>
+                     {
+                         h.Username("guest");
+                         h.Password("guest");
+                     });
+                 }
+               )
+              )
+             );
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
