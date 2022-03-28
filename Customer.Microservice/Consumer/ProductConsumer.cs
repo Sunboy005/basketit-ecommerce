ead99f792921742c4adba583c9b043c82d55238d
Amos Alagbe <sunboy005@gmail.com>
2022-03-28 23:33:54 +0100

    Completed Microservices

---
+++ Customer.Microservice/Consumer/ProductConsumer.cs
@@ -0,0 +1,14 @@
+﻿using MassTransit;
+using Share.Models.Models;
+using System.Threading.Tasks;
+
+namespace Customer.Microservice.Consumer
+{
+    public class ProductConsumer : IConsumer<CustomerProduct>
+    {
+        public async Task Consume(ConsumeContext<CustomerProduct> context)
+        {
+            await Task.Run(() => { var obj = context.Message; });
+        }
+    }
+}
---
+++ Share.Models/Models/CustomerProduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Share.Models.Models
+{
+    public class CustomerProduct
+    {
+        public string ProductName { get; set; } 
+        public string ProductDescription { get; set; }
+        public DateTime DateCreated { get; set; }
+        public decimal Price { get; set; }
+    }
+}
